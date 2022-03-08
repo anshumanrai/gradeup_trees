25b7a1bd7a6b87e39d497158a5a40b1b8579c6eb
Anshuman Rai <anshuman.rai78@gmail.com>
2022-03-08 02:16:13 -0800

    read input file in BinaryTree.java

--- src/main/java/edu/marist/ds/BinaryTree.java
+++ src/main/java/edu/marist/ds/BinaryTree.java
@@ -1,5 +1,8 @@
 package edu.marist.ds;
 
+import java.io.*;
+import java.util.ArrayList;
+
 
 class BTNode {
 
@@ -9,9 +12,47 @@ class BT {
 
 	String inpFileName;
 
+	ArrayList<String> inpStrings;
+
+	public void readInputFile() {
+
+		try {
+		  
+			File file=new File(inpFileName);    //creates a new file instance  
+
+			FileReader fr=new FileReader(file);   //reads the file  
+
+			BufferedReader br=new BufferedReader(fr);  //creates a buffering character input stream  
+
+			String line;  
+
+			while((line=br.readLine())!=null)  
+			{  
+
+				System.out.println(line);
+
+				inpStrings.add(line);
+			
+			}  
+
+			fr.close();    //closes the stream and release the resources  
+		}  
+		catch(IOException e)  
+		{  
+
+			e.printStackTrace();  
+
+		}  
+
+		return;
+
+	}	
+
 	public BT(String inInpFileName) {
 
 		inpFileName = inInpFileName;
+
+		inpStrings = new ArrayList<String>();
 
 		System.out.println("BT: inpFileName : " + inpFileName);
 
@@ -27,6 +68,8 @@ public class BinaryTree {
 	public static void main (String args[]) {
 
 		BT bt = new BT(args[0]);
+
+		bt.readInputFile();
 
 		System.out.println("Running BinaryTree");
 
